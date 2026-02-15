78893f073122dd2ae99af2d8dc466b3c0d8de2d7
𝐒𝐚𝐥𝐞𝐡 <2001739463@jolearn.jo>
2026-02-15 20:13:14 +0300

    Update a.js

--- a.js
+++ a.js
@@ -1,1 +1,12 @@
+  const correctPassword = "1234";
 
+  function login(){
+    const pass = document.getElementById("password").value;
+    const error = document.getElementById("error");
+
+    if(pass === correctPassword){
+      window.location.href = "web.html";
+    } else {
+      error.innerHTML = "الباسورد غير صحيح";
+    }
+  }
